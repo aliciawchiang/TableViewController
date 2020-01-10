8e9b320be5e743045579daa97fec1a4c768f553d
グエン ビエン <viennguyen@guens-MacBook-Pro.local>
2020-01-11 00:43:46 +0900

    Add demo load data to tableViewCell by xib file.

--- TableViewController/CustomCell.swift
+++ TableViewController/CustomCell.swift
@@ -9,9 +9,21 @@
 import Foundation
 import UIKit
 
+struct CellData {
+    var title: String!
+    var image: String!
+    init(title: String = "", image: String = "") {
+        self.title = title
+        self.image = image
+    }
+}
+
 class CustomCell: UITableViewCell {
     @IBOutlet var picImage: UIImageView!
     @IBOutlet var label: UILabel!
     
-    
+    func configCell(data: CellData) {
+        self.label.text = data.title
+        self.picImage.image = UIImage(named: data.image)
+    }
 }
--- TableViewController/ViewController.swift
+++ TableViewController/ViewController.swift
@@ -9,21 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var names = ["Hello"]
-    var pic = ["pic"]
+    var data : [CellData] = []
     
     @IBOutlet var tabelView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.tabelView.register(UINib(nibName: "CustomTableViewCell", bundle: nil), forCellReuseIdentifier: "myCell")
     }
     @IBAction func addItem(_ sender: Any)
     {
         var textField = UITextField()
         let alert = UIAlertController(title: "Add New Entry", message: "", preferredStyle: .alert)
         let actionAdd = UIAlertAction(title: "Add", style: .destructive, handler: {(action) in
-            self.names.append(textField.text!)
+            //self.names.append(textField.text!)
+            self.data.append(CellData(title: textField.text!, image: "huongduong01"))
+            self.tabelView.reloadData()
         })
         let actionCancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
         
@@ -40,32 +41,35 @@ class ViewController: UIViewController {
 }
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return names.count
+        return self.data.count
     }
     func numberOfSections(in tableView: UITableView) -> Int {
-        return names.count
+        return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! CustomCell
-        cell.label?.text = names[indexPath.row]
-        cell.picImage.image = UIImage(named: pic[indexPath.row])
-        cell.layer.cornerRadius = 12
-        cell.layer.borderWidth = 1
-        cell.layer.borderColor = UIColor.darkGray.cgColor
-        cell.clipsToBounds = true
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell") as! CustomCell
+//        cell.label?.text = names[indexPath.row]
+//        cell.picImage.image = UIImage(named: pic[indexPath.row])
+//        cell.layer.cornerRadius = 12
+//        cell.layer.borderWidth = 1
+//        cell.layer.borderColor = UIColor.darkGray.cgColor
+//        cell.clipsToBounds = true
+        if self.data.count > 0 {
+            cell.configCell(data: data[indexPath.row])
+        }
         return cell
     }
     
 }
 extension ViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 250
-    }
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let view = UIView()
-        view.backgroundColor = .clear
-        return view
-    }
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return 250
+//    }
+//    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+//        let view = UIView()
+//        view.backgroundColor = .clear
+//        return view
+//    }
 }
 
